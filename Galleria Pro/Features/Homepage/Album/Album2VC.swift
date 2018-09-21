c6124517f069ed1af9b0a04d0b0f77a9abbebf7f
Giorgio Sertori <giorgiosertori32@gmail.com>
2018-09-21 23:48:34 +0200

    Inserita homepage con i 3 tab per i 3 album

---
+++ Galleria Pro/Features/Homepage/Album/Album2VC.swift
@@ -0,0 +1,30 @@
+//
+//  Album2.swift
+//  Galleria Pro
+//
+//  Created by Giorgio Sertori on 21/09/18.
+//  Copyright © 2018 Giorgio Sertori. All rights reserved.
+//
+
+import UIKit
+import XLPagerTabStrip
+
+class Album2VC: UIViewController, IndicatorInfoProvider {
+    
+    //MARK: Functions
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+        return IndicatorInfo(title: "Album 2")
+    }
+    
+}
--- Galleria Pro/Features/Homepage/HomepageVC.swift
+++ Galleria Pro/Features/Homepage/HomepageVC.swift
@@ -7,12 +7,32 @@
 //
 
 import UIKit
+import XLPagerTabStrip
 
-class HomepageVC: UIViewController{
+class HomepageVC: ButtonBarPagerTabStripViewController{
     
+    
+    let purpleInspireColor = UIColor(red:0.13, green:0.03, blue:0.25, alpha:1.0)
     //MARK: Functions
     
     override func viewDidLoad() {
+        
+        // change selected bar color
+        settings.style.buttonBarBackgroundColor = .white
+        settings.style.buttonBarItemBackgroundColor = .white
+        settings.style.selectedBarBackgroundColor = purpleInspireColor
+        settings.style.buttonBarItemFont = .boldSystemFont(ofSize: 14)
+        settings.style.selectedBarHeight = 2.0
+        settings.style.buttonBarMinimumLineSpacing = 0
+        settings.style.buttonBarItemTitleColor = .black
+        settings.style.buttonBarItemsShouldFillAvailiableWidth = true
+        settings.style.buttonBarLeftContentInset = 0
+        settings.style.buttonBarRightContentInset = 0
+        changeCurrentIndexProgressive = { [weak self] (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
+            guard changeCurrentIndex == true else { return }
+            oldCell?.label.textColor = .black
+            newCell?.label.textColor = self?.purpleInspireColor
+        }
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -20,6 +40,13 @@ class HomepageVC: UIViewController{
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewControllers(for pagerTabStripController: PagerTabStripViewController) -> [UIViewController] {
+        let album_1 = UIStoryboard(name: "Homepage", bundle: nil).instantiateViewController(withIdentifier: "Album1VC")
+        let album_2 = UIStoryboard(name: "Homepage", bundle: nil).instantiateViewController(withIdentifier: "Album2VC")
+        let album_3 = UIStoryboard(name: "Homepage", bundle: nil).instantiateViewController(withIdentifier: "Album3VC")
+        return [album_1, album_2, album_3]
     }
 
     
