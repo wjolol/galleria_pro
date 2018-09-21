7f2bac6a33357aa056ad300aa0ed2509190562a1
Giorgio Sertori <giorgio@MacBook-Pro-di-Giorgio.local>
2018-09-21 18:34:08 +0200

    first commit

---
+++ Galleria Pro/Features/Impostazioni/ImpostazioniVM.swift
@@ -0,0 +1,9 @@
+//
+//  ImpostazioniVM.swift
+//  Galleria Pro
+//
+//  Created by Giorgio Sertori on 21/09/18.
+//  Copyright © 2018 Giorgio Sertori. All rights reserved.
+//
+
+import Foundation
---
+++ Galleria Pro/Services/getPhotoApiService.swift
@@ -0,0 +1,9 @@
+//
+//  getPhotoApiService.swift
+//  Galleria Pro
+//
+//  Created by Giorgio Sertori on 21/09/18.
+//  Copyright © 2018 Giorgio Sertori. All rights reserved.
+//
+
+import Foundation
