a9b6973ff9e5469e062440af007ea439b8ddc88f
Giorgio Sertori <giorgio@macbook-pro-di-giorgio.local>
2018-09-21 18:13:05 +0200

    Initial Commit

---
+++ Galleria Pro/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Galleria Pro
+//
+//  Created by Giorgio Sertori on 21/09/18.
+//  Copyright © 2018 Giorgio Sertori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
